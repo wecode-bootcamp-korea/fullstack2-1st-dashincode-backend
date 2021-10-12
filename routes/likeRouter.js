0f3f04517d4ccb805d8e118211e4caf7af9c9466
dabin <dabin219@naver.com>
2021-10-12 13:52:54 +0900

    Fix: get과 post 분리

--- routes/likeRouter.js
+++ routes/likeRouter.js
@@ -4,7 +4,7 @@ import { likeController } from '../controllers';
 const router = express.Router();
 
 router.get('/:id', likeController.getLike);
-router.get('/:id', likeController.likeProduct);
+router.post('/:id', likeController.likeProduct);
 //token validation 미들웨어 필요
 
 export default router;
