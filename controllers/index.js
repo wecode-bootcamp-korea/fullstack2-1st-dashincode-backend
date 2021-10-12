410fca57484498bc93c4f5af79444876ed2084f4
dabin <dabin219@naver.com>
2021-10-12 18:28:48 +0900

    Fix: product 관련 파일 삭제, search와 list 합침

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,17 @@
-import categoryController from './categoryController';
 import productController from './productController';
 import commentController from './commentController';
 import cartController from './cartController';
 import likeController from './likeController';
 import loginController from './loginController';
 import signupController from './signupController';
-import searchController from './searchController';
 import listController from './listController';
 
 export {
-  categoryController,
   productController,
   commentController,
   cartController,
   likeController,
   loginController,
   signupController,
-  searchController,
   listController,
 };
--- models/index.js
+++ models/index.js
@@ -1,21 +1,17 @@
-import categoryDao from './categoryDao';
 import productDao from './productDao';
 import commentDao from './commentDao';
 import cartDao from './cartDao';
 import likeDao from './likeDao';
 import loginDao from './loginDao';
 import signupDao from './signupDao';
-import searchDao from './searchDao';
 import listDao from './listDao';
 
 export {
-  categoryDao,
   productDao,
   commentDao,
   cartDao,
   likeDao,
   loginDao,
   signupDao,
-  searchDao,
   listDao,
 };
