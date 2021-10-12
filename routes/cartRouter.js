42fe7471a5eab639f43b38045065d77c0263c9b9
jihnk <jhyunk218@gmail.com>
2021-10-12 17:10:02 +0900

    fix:prisma 수정 후 머지

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -3,9 +3,9 @@ import { cartController } from '../controllers';
 
 const router = express.Router();
 
-router.post('/:id', cartController.addCartList);
+router.post('/', cartController.addCartList);
 router.get('/', cartController.getCartList);
-router.put('/update/:id', cartController.updateCartList);
+router.put('/:id', cartController.updateCartList);
 router.delete('/:id', cartController.deleteCartList);
 
 export default router;
