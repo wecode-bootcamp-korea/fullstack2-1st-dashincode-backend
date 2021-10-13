935796de74f8c539a6d666305ebc4ec1dd73ca30
jungho shin <junghoshin@junghoui-MacBookAir.local>
2021-10-13 14:30:06 +0900

    Fix: conflict 해결2

--- models/index.js
+++ models/index.js
@@ -5,17 +5,4 @@ import cartDao from './cartDao';
 import likeDao from './likeDao';
 import listDao from './listDao';
 
-<<<<<<< HEAD
-export {
-  productDao,
-  commentDao,
-  cartDao,
-  likeDao,
-  loginDao,
-  signupDao,
-  searchDao,
-  listDao,
-};
-=======
 export { productDao, commentDao, cartDao, likeDao, userDao, listDao };
->>>>>>> main
