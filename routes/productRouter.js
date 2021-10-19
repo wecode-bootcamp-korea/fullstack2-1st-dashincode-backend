27b61c5ce80efbaa4c8ef952d184d93e4d89200e
jambottle <jaewon.walks@gmail.com>
2021-10-19 19:15:55 +0900

    Remove: 불필요하게 입력한 주석 삭제

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -10,8 +10,6 @@ router.get('/:id/info', productController.getProductInfo);
 router.get('/:id/thumbnail', productController.getProductThumbnails);
 router.get('/:id/image', productController.getProductDescriptionImage);
 router.get('/:id/review', productController.getProductReviewList);
-
-//like API
 router.get('/:id/like', productController.getLikedProduct);
 router.post('/:id/like', productController.likeProduct);
 router.delete('/:id/like', productController.unlikeProduct);
